56772529785eff43545d87cd2b483a799e5403e6
lumanyano654 <gomomolumanyano@gmail.com>
2020-12-10 16:59:47 +0200

    added records

--- public/js/langsound.js
+++ public/js/langsound.js
@@ -9,15 +9,27 @@ let sotho = async () => {
     var isisuthu = new Audio('sounds/isisuthu.mp3');
     isisuthu.play()
 }
-
-function venda(){
-    var isivenda = new Audio('sounds/venda.mp3');
-    isivenda.play()
+let venda = async () => {
+    var tshiVenda = new Audio('sounds/venda.mp3');
+    tshiVenda.play()
 }
+
+
 
 let ndebele = async () => {
     var isindebele = new Audio('sounds/isindebele.mp3');
-    isindebele.play()
+    isindebele.play();
+    
+}
+
+let xitsonga = async () => {
+    var xitsonga = new Audio('sounds/xitsonga.mp3');
+    xitsonga.play()
+}
+
+let afrikaans = async () => {
+    var afrikaans = new Audio('sounds/afrikaans.mp3');
+    afrikaans.play()
 }
 
 
@@ -27,6 +39,6 @@ document.addEventListener("click", sound());
 document.addEventListener("click", sotho());
 document.addEventListener("click", venda());
 document.addEventListener("click", ndebele());
+document.addEventListener("click", afrikaans());
+document.addEventListener("click", xitsonga());
 
-
-// playLang.innerHTML = bell.play()
